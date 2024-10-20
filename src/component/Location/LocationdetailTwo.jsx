b40664719fe95dfb2c02d83da59c0762fa5c8829
mdnahid47 <nmd97243@gmail.com>
2024-10-20 15:24:04 +0600

    added

--- src/component/Location/LocationdetailTwo.jsx
+++ src/component/Location/LocationdetailTwo.jsx
@@ -25,7 +25,7 @@ const LocationdetailTwo = () => {
           </a>
         </div>
 
-        <div className="mt-2 flex flex-col md:flex-row justify-between bg-[#f4f4f5] px-4 py-3 rounded transition-transform duration-300 transform hover:scale-105">
+        <div className="mt-2 flex flex-col md:flex-row justify-between bg-[#f4f4f5] px-4 py-3 rounded transition-transform duration-300 transform hover:scale-105 cursor-pointer">
           <div className="flex gap-4 items-center mt-3">
             <img src={HomePic} alt="" className="w-32" />
             <div className="space-y-1">
