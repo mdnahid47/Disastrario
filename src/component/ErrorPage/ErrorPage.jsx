547962454a50524763a9b86127c5c77fdb01cf20
mdnahid47 <nmd97243@gmail.com>
2024-10-20 14:43:33 +0600

    modify error page

--- src/component/ErrorPage/ErrorPage.jsx
+++ src/component/ErrorPage/ErrorPage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const ErrorPage = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 gap-5">
         <h1 className="text-9xl font-bold text-gray-800 mb-4">404</h1>
         <h2 className="text-3xl font-semibold text-gray-700 mb-4">
           Oops! Page not found.
