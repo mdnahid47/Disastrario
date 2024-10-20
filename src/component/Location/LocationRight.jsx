133a9684ed81a9994780c1a1543fb66da92458f1
mdnahid47 <nmd97243@gmail.com>
2024-10-20 14:45:04 +0600

    cursor add

--- src/component/Location/LocationRight.jsx
+++ src/component/Location/LocationRight.jsx
@@ -4,7 +4,7 @@ const LocationRight = () => {
   return (
     <div>
       <p className="text-sm text-secondary mb-2 font-semibold">Incident Map</p>
-      <img src={MapOne} alt="" />
+      <img src={MapOne} alt=""  className="cursor-pointer"/>
       <div className="flex justify-start gap-10 py-2">
         <p className="text-sm text-secondary">Start 19.1232, -118.233</p>
         <p className="text-sm text-secondary">End 19.3245, -119.2323</p>
